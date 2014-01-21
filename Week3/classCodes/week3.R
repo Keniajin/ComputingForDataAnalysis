aed7ae7062ea256fdfa9a05f12e807e930591bb5
Ken <keniajin@gmail.com>
2014-01-21 21:17:04 +0300

    course update

--- Week3/classCodes/week3.R
+++ Week3/classCodes/week3.R
@@ -12,3 +12,22 @@ e <- rnorm(100, 0, 1)
 y <- 1.5 - 3 * x + e
 plot(x,y)
 
+x <- rnorm(100)
+hist(x)
+dev.off()
+
+x <- rnorm(100)
+y <- x + rnorm(100)
+par(las = 1)
+plot(x, y)
+#las sets the orientation of the x labels or orientation of the labels
+par(las = 2)
+plot(x, y)
+
+x <- rnorm(100)
+y <- x + rnorm(100)
+plot(x, y)
+
+x1 <- rnorm(10)
+y1 <- rnorm(10)
+points(x1, y1, col = "red")
